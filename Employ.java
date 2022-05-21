28021be4bf06f01e424ea71d0eed72c0e3c47ec8
idlmhor med <105658089+idmed22@users.noreply.github.com>
2022-05-21 15:26:50 +0100

    Update Employ.java

--- Employ.java
+++ Employ.java
@@ -1,56 +1,38 @@
 package sal.Em.Jv;
 
-public class Employ {
-  private String nom;
-  private String prenom;
-  private String  date;
-  double salaire;
-  public Employ (String nom,String prenom,String date) {
-  this.nom = nom;
-  this.prenom = prenom;
-  this.date = date;
- 
-  }
-  public Double CalSalaire(Double sal){
-		 return sal;
-	 }
-//getter 
-  public String getName() {
-	  return nom;
-	  
-  }
-  public String getPrenom() {
-	  return prenom;
-	  
-  }
-   
- public String getDate() {
-	 return date;
- }
- 
- 
- public String getNom() {
-	return nom;
-}
-
-public void setNom(String nom) {
-	this.nom = nom;
-}
-
-public void setPrenom(String prenom) {
-	this.prenom = prenom;
-}
-
-public void setDate(String date) {
-	this.date = date;
-}
-public String toString() {
-	return "nom=" + nom + ", prenom=" + prenom + ", date=" + date;
-}
-
-
+public class Empl {
+	  String nom;
+	  String prenom;
+	  String  date;
+	  public Empl (String nom,String prenom,String date) {
+	  this.nom = nom;
+	  this.prenom = prenom;
+	  this.date = date;
 	 
-	 
- 
+	  }
+	  	  
+	  
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String  toString() {
+		return  "nom :" + getNom() + "\n prenom :" + getPrenom() + "\n date :" + getDate();
+
+	}
 
 }
