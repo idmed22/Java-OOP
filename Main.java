2e115a25639e2dc9792e33e64bb4ddc044054f2e
idlmhor med <105658089+idmed22@users.noreply.github.com>
2022-05-21 09:57:16 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,14 +5,16 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//Employ e1 = new Employ("Mohamed", "Beta", "2000-10-20");
+		// empf
 		EmpFixe empf = new EmpFixe("Mohamed", "Beta", "2000-10-20", 4000.0);
 		System.out.println(empf);
 		// emp2
-		EmpFixe emp2 =new EmpFixe("Said", "Alpha", "2003-16-20",5034, null);
+		EmpFixe emp2 =new EmpFixe("Said", "Alpha", "2003-16-20",5034.0);
+		System.out.println(emp2);
 		
 		// emp3
-		EmpFixe emp3 =new EmpFixe("Ahmed", "Beta", "2013-05-12",4500, null);
+		EmpFixe emp3 =new EmpFixe("Ahmed", "Beta", "2013-05-12",4500.0);
+		System.out.println(emp3);
 		
 	
 		
